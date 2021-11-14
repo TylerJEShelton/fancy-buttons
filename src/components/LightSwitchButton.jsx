cbf297e9860b54baedd848b94523781827a58e3f
Tyler <tylershelton99@gmail.com>
2021-11-14 16:08:24 -0500

    Add 4 button components, state, dark mode and functionality to have each button interactable

---
+++ src/components/LightSwitchButton.jsx
@@ -0,0 +1,14 @@
+function LightSwitchButton(props) {
+  const {light, switchLight} = props;
+  
+  const handleClick = () => switchLight();
+
+  return (
+    <button className="LightSwitchButton" onClick={handleClick}>
+      {light === "on" && <span className="on"><i>💡</i> I'm on!</span>}
+      {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
+    </button>
+  );
+}
+
+export default LightSwitchButton;
